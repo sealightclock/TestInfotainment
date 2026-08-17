c11c5e46fc2e0fd5d738328cb8c43556f322c7d3
Sealight Clock <chbebram@hotmail.com>
2026-08-17 13:55:51 -0700

    1st push: Dummy Android Studio project with Android 17, Kotlin, build.gradle.kts, and libs.versions.toml

---
+++ app/src/main/java/com/example/jonathan/testinfotainment/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.jonathan.testinfotainment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jonathan.testinfotainment.ui.theme.TestInfotainmentTheme
+
+private const val TAG = "TIF: MainActivity"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "onCreate")
+
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContent {
+            TestInfotainmentTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestInfotainmentTheme {
+        Greeting("Android")
+    }
+}
